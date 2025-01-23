6dfb19be14dee97e5f07b076469f8b78b580267e
Dan Freeman <freebird@dans-mbp.mynetworksettings.com>
2025-01-22 21:02:18 -0800

    rename and labels

--- R/crux-analyze.R
+++ R/crux-analyze.R
@@ -6,8 +6,6 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 
-# print(getwd()) 
-
 january_data <- read.csv("data/all-202401.csv")
 february_data <- read.csv("data/all-202402.csv")
 march_data <- read.csv("data/all-202403.csv")
@@ -15,13 +13,12 @@ march_data <- read.csv("data/all-202403.csv")
 
 all_data <- rbind(january_data, february_data, march_data)
 
-
+# clean data, we only want to deal with desktop and companies that have 3 months of data
 all_data <- all_data %>% filter(device != "phone") 
 all_data <- all_data %>% filter(device != "tablet") 
 all_data <- all_data %>% filter(origin != "https://www.on.com") 
 all_data <- all_data %>% filter(origin != "https://www.hoka.com") 
 
-# View(all_data)
 
 # add an actual date column to use as an x-axis
 all_data$date_type <- ym(all_data$yyyymm)
@@ -29,7 +26,19 @@ print(all_data)
 
 ggplot(all_data, aes(x=date_type, y=p75_fcp, group=origin, color=origin)) +
   geom_line() +
-  scale_x_date(date_labels = "%B %Y", date_breaks = "1 month")
+  theme(
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", color = NA), 
+    panel.background = element_rect(fill = "white", color = NA),
+    legend.text = element_text(size = 11, family = "Arial", face = "bold")) +
+  guides(color = guide_legend(ncol = 2)) +
+  geom_line(size = 1.5) + # Thicker lines  
+  scale_x_date(date_labels = "%B %Y", date_breaks = "1 month") +
+  labs(
+    x = "Month",
+    y = "FCP",
+    title = "First Contentful Paint - p75th"
+  ) 
 
 ggplot(all_data, aes(x=date_type, y=p75_lcp, group=origin, color=origin)) +
   geom_line() +
@@ -44,7 +53,7 @@ ggplot(all_data, aes(x=date_type, y=p75_lcp, group=origin, color=origin)) +
   labs(
     x = "Month",
     y = "LCP",
-    title = "LCP by Origin at p75th"
+    title = "Largest Contentful Paint - p75th"
   ) 
 
   
@@ -61,7 +70,7 @@ ggplot(all_data, aes(x=date_type, y=p75_ttfb, group=origin, color=origin)) +
   labs(
     x = "Month",
     y = "TTFB",
-    title = "TTFB by Origin at p75th"
+    title = "Time to First Byte - p75th"
   ) 
 
 
@@ -101,12 +110,12 @@ max_value2 <- max(all_grouped_average_data$avg_fast_lcp)
 ggplot(all_grouped_average_data_long, aes(x = origin, y = mean_value, fill = variable)) +
   geom_bar(stat = "identity", position = "dodge") +  # Position bars side by side
   scale_x_discrete(labels = function(x) lookup(x)) +  # Rename x-axis labels using the lookup function
-  labs(title = "Average Values by Group", x = "Group", y = "Average Value") +
+  labs(title = "LCP vs Percentage of Fast LCP", x = "Group", y = "Average Value") +
   scale_y_continuous(
-    name = "Average Value", 
+    name = "Average LCP", 
     sec.axis = sec_axis(
       trans = ~ . / 10000, 
-      name = "Normalized Average Value2"
+      name = "Average Percentage of Fast LCP"
     )  # Secondary y-axis for 'avg_value2'
   ) +
   theme_minimal()
@@ -123,6 +132,6 @@ all_average_4g_data <- all_data %>%
 ggplot(all_average_4g_data, aes(x = origin, y = avg_value, fill = origin)) +
   geom_bar(stat = "identity") +
   scale_x_discrete(labels = function(x) lookup(x)) + 
-  labs(title = "Percent of 4g Users", x = "Origin", y = "Percentage") +
+  labs(title = "Percentage of 4g Users", x = "Origin", y = "Percentage") +
   theme_minimal()
 
